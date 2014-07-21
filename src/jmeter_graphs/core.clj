8b5e9ea6f4d00f3253cc4f752c84266edd6a0b53
AidyLewis <adrian_lewis@fastmail.fm>
2014-07-21 18:19:51 +0100

    ready to jar it up and using a header

--- src/jmeter_graphs/core.clj
+++ src/jmeter_graphs/core.clj
@@ -1,10 +1,11 @@
 (ns jmeter-graphs.core
 (:require [incanter.core :as incanter]
           [incanter.io :as io]
-          [incanter.charts :as charts]))
+          [incanter.charts :as charts])
+(:gen-class))
 
 (defn read-csv [filepath]
-  (io/read-dataset filepath))
+  (io/read-dataset filepath :header true))
 
 (defn graph-response [dataset title]
   (incanter/view (charts/time-series-plot
@@ -16,31 +17,15 @@
 
 (defn graph-response-report [filepath title]
   (let [dataset (read-csv filepath)]
-    (graph dataset title)))
+    (graph-response dataset title)))
+
+(defn -main [& args]
+  (when (= (first args) "response") 
+    (graph-response-report (second args) (nth args 2))))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+;;;
+(def epochs '(1405060620301 1405060625735 1405060626141
+                                 1405060625559 1405060627285 1405060626927 1405060625174 1405060626962 140506062616))
 
 

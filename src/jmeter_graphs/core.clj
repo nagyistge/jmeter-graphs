3719aeb91403d271d55fbe9672c8dbb08b2f743f
AidyLewis <adrian_lewis@fastmail.fm>
2014-07-23 11:08:09 +0100

    changed function signature for graph-report

--- src/jmeter_graphs/core.clj
+++ src/jmeter_graphs/core.clj
@@ -5,32 +5,35 @@
 (:gen-class))
 
 (defn read-csv [filepath]
-  (io/read-dataset filepath :header true))
+  (io/read-dataset filepath))
 
-(defn graph-response [dataset title]
+(defn graph-response [dataset title y-label]
   (incanter/view (charts/time-series-plot
                   (incanter/sel dataset :cols 0)
                   (incanter/sel dataset :cols 1)
                   :x-label "minutes"
-                  :y-label "response time (ms)"
+                  :y-label y-label
                   :title title)))
 
 (defn graph-response-report [filepath title]
   (let [dataset (read-csv filepath)]
     (graph-response dataset title)))
 
-(defn number-of-requests-per-second [filepath]
+(defn number-of-requests-in-a-second [filepath]
   (let [dataset (read-csv filepath)
         epochs (incanter/sel dataset :cols 0)
-        epoch-map (reduce-kv (fn [c k v] (assoc c k (count v))) {} (group-by #(quot % 1000) epochs))
-        first-epoch (incanter/sel dataset :rows 0 :cols 0)
-        last-epoch (incanter/sel dataset :rows (- (incanter/nrow dataset) 1) :cols 0)]))
+        epoch-map (reduce-kv (fn [c k v] (assoc c k (count v))) {} (group-by #(quot % 1000) epochs))] 
+    (sort epoch-map)))
 
-(defn graph-requests-per-second [filepath title])
+(defn request-dataset [filepath] 
+   (incanter/dataset ["time" "requests"] 
+                     (number-of-requests-in-a-second filepath)))
+
+(defn graph-request-report [filepath title])
 
 ;;; Examples
 ;;; (-main :graph "response" :filepath "test/jmeter_graphs/aggregate-report.csv" :title "20rps")
 (defn -main [& args]
   (let [{:keys [graph filepath title]} args]
     (when (= graph "response")
-      (graph-response-report filepath title))))
+      (graph-response-report filepath title "response time (ms)"))))

6baf07f95d3d8bd6c0116575f2a011572b03634d
AidyLewis <adrian_lewis@fastmail.fm>
2014-07-24 16:42:44 +0100

    throughput plus sample

--- src/jmeter_graphs/core.clj
+++ src/jmeter_graphs/core.clj
@@ -20,7 +20,6 @@
   (let [dataset (read-csv filepath)]
     (graph-dataset dataset title "Response Times (ms)")))
 
-
 ;;; request graph
 (defn number-of-requests-in-a-second [filepath]
   (let [dataset (read-csv filepath)
@@ -28,13 +27,13 @@
         seconds-map (reduce-kv (fn [c k v] (assoc c k (count v))) {} (group-by #(quot % 1000) epochs))] 
     seconds-map))
 
-(defn convert-seconds-to-milliseconds [filepath]
-  (let [seconds-map (number-of-requests-in-a-second filepath)] 
-    (apply merge 
-           (map (fn [[k v]] {(* k 1000) v}) seconds-map))))
+(defn convert-seconds-to-milliseconds [seconds]
+  (apply merge 
+         (map (fn [[k v]] {(* k 1000) v}) seconds)))
 
 (defn request-dataset [filepath] 
-  (let [sorted-dataset (sort (convert-seconds-to-milliseconds filepath))]
+  (let [requests-per-second (number-of-requests-in-a-second filepath)
+        sorted-dataset (sort (convert-seconds-to-milliseconds requests-per-second))]
     (incanter/dataset ["time" "requests"] 
                       sorted-dataset)))
 
@@ -49,12 +48,29 @@
         col1 (incanter/sel dataset :cols 1)]
     (map + col0 col1)))
 
+(defn throughput-by-second [filepath]
+  (let [throughput (sum-epoch-and-elapsed-times filepath)
+     seconds-map (reduce-kv (fn [c k v] (assoc c k (count v))) {} (group-by #(quot % 1000) throughput))]
+    seconds-map))
+
+(defn throughput-dataset [filepath] 
+  (let [requests-per-second (throughput-by-second filepath)
+        sorted-dataset (sort (convert-seconds-to-milliseconds requests-per-second))]
+    (incanter/dataset ["time" "throughput"] 
+                      sorted-dataset)))
+
+(defn graph-throughput [filepath]
+  (let [dataset (throughput-dataset filepath)]
+    (graph-dataset dataset "Throughput" "Throughout Per Second")))
+
 ;;; Examples
 ;;; (-main :graph "response" :filepath "test/jmeter_graphs/fixtures/aggregate-report.csv" :title "20rps")
 ;;; (-main :graph "requests" :filepath "test/jmeter_graphs/fixtures/aggregate-report.csv")
+;;; (-main :graph "throughput" :filepath "test/jmeter_graphs/fixtures/aggregate-report.csv")
 (defn -main [& args]
   (let [{:keys [graph filepath title]} args]
     (cond 
      (= graph "response") (graph-response filepath title)
-     (= graph "requests") (graph-request filepath))))
+     (= graph "requests") (graph-request filepath)
+     (= graph "throughput") (graph-throughput filepath))))
 

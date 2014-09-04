f6965c1e7be84792edfe24b9c561ed3607b26f0a
AidyLewis <adrian_lewis@fastmail.fm>
2014-09-04 10:45:27 +0100

    bumped up version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jmeter-graphs "0.1.0"
+(defproject jmeter-graphs "0.1.1"
   :description "A utility to graph from JMeter CSV logs"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
